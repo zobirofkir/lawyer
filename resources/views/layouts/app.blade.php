cd299aff1e115a2990d6871633096715dbe074ce
zobirofkir <zobirofkir19@gmail.com>
2024-11-14 17:31:30 +0100

    create head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,31 +1,63 @@
 <!doctype html>
 <html class="no-js" lang="zxx">
 <head>
-<meta charset="utf-8">
-<meta http-equiv="x-ua-compatible" content="ie=edge">
-<title>Rachid Rethmani</title>
-<meta name="description" content>
-<link rel="shortcut icon" type="image/x-icon" href="assets/img/icon/favicon.png">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    
+    <!-- English Meta Tags -->
+    <title>Rachid Rethmani - Expert Lawyer & Legal Consultant</title>
+    <meta name="description" content="Rachid Rethmani is a trusted and experienced lawyer providing top-notch legal advice and representation. Specializing in civil, criminal, and corporate law.">
+    <meta name="keywords" content="lawyer, legal consultant, Rachid Rethmani, civil law, criminal law, corporate law, legal services, attorney">
+    
+    <!-- French Meta Tags -->
+    <meta name="description" lang="fr" content="Rachid Rethmani est un avocat de confiance et expérimenté offrant des conseils juridiques et une représentation de premier ordre. Spécialisé en droit civil, pénal et des affaires.">
+    <meta name="keywords" lang="fr" content="avocat, consultant juridique, Rachid Rethmani, droit civil, droit pénal, droit des affaires, services juridiques, avocat">
+    
+    <meta name="author" content="Rachid Rethmani">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Rachid Rethmani - Expert Lawyer & Legal Consultant">
+    <meta property="og:description" content="Trusted and experienced lawyer offering exceptional legal services in civil, criminal, and corporate law.">
+    <meta property="og:image" content="{{asset('assets/img/icon/favicon.png')}}">
+    <meta property="og:url" content="{{asset('assets/img/icon/favicon.png')}}">
+    <meta property="og:site_name" content="Rachid Rethmani Legal Services">
+    
+    <!-- French Open Graph / Facebook -->
+    <meta property="og:title" lang="fr" content="Rachid Rethmani - Avocat Expert & Consultant Juridique">
+    <meta property="og:description" lang="fr" content="Avocat de confiance et expérimenté offrant des services juridiques exceptionnels en droit civil, pénal et des affaires.">
+    
+    <!-- Twitter -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Rachid Rethmani - Expert Lawyer & Legal Consultant">
+    <meta name="twitter:description" content="Providing top-notch legal advice and representation in civil, criminal, and corporate law.">
+    <meta name="twitter:image" content="{{asset('assets/img/icon/favicon.png')}}">
+    
+    <!-- French Twitter Meta Tags -->
+    <meta name="twitter:title" lang="fr" content="Rachid Rethmani - Avocat Expert & Consultant Juridique">
+    <meta name="twitter:description" lang="fr" content="Offrant des conseils juridiques et une représentation de premier ordre en droit civil, pénal et des affaires.">
+    
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/icon/favicon.png">
+    
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/slicknav.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/animate.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/fontawesome-all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/slick.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/nice-select.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link rel="stylesheet" href="{{asset('assets/css/modal.css')}}">  
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('assets/css/layout.css')}}">
+</head>    
 
-<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
-<link rel="stylesheet" href="{{asset('assets/css/owl.carousel.min.css')}}">
-<link rel="stylesheet" href="{{asset('assets/css/slicknav.css')}}">
-<link rel="stylesheet" href="{{asset('assets/css/animate.min.css')}}">
-<link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
-<link rel="stylesheet" href="{{asset('assets/css/fontawesome-all.min.css')}}">
-
-<link rel="stylesheet" href="{{asset('assets/css/themify-icons.css')}}">
-<link rel="stylesheet" href="{{asset('assets/css/slick.css')}}">
-<link rel="stylesheet" href="{{asset('assets/css/nice-select.css')}}">
-<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-<link rel="stylesheet" href="{{asset('assets/css/modal.css')}}">  
-<!-- Bootstrap CSS -->
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="{{asset('assets/css/layout.css')}}">
-</head>
-<body>
+    <body>
 
     <div>
         {{ $slot }}
